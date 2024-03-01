878898d146ad0267c7ea6a5e05e38d9b75612cef
movedx <grytsenko.bogdan@gmail.com>
2024-03-01 20:42:36 +0100

    change port

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from flask import Flask, jsonify, Response
 
 def create_app():
   app = Flask(__name__)
-  # Error 404 handler
+
   @app.errorhandler(404)
   def resource_not_found(e):
     return jsonify(error=str(e)), 404
-  # Error 405 handler
+
   @app.errorhandler(405)
   def resource_not_found(e):
     return jsonify(error=str(e)), 405
-  # Error 401 handler
+
   @app.errorhandler(401)
   def custom_401(error):
     return Response("API Key required.", 401)
@@ -27,4 +27,4 @@ app = create_app()
 if __name__ == "__main__":
   #    app = create_app()
   print(" Starting app...")
-  app.run(host="0.0.0.0", port=5000)
+  app.run(host="0.0.0.0", port=5555)
